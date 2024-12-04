dbb25dc029779d1ccf3c7030dfaea66b5d66fa10
Adam Bastos Santos <adam.santos@anvisa.gov.br>
2024-12-04 18:31:20 -0300

    Exercicio String

---
+++ HASHTAG_PY/exercicios/exerstring.py
@@ -0,0 +1,7 @@
+#Inserir o CPF apenas números e, caso tenha algo diferente de números ou for menor de 11 números, avisa um erro
+
+cpf = input("Digite seu CPF (Apenas números): ")
+if (not cpf.isnumeric() or len(cpf) != 11):
+    print("Digite seu CPF corretamente e apenas números.")
+else:
+    print("Tudo certo com seu CPF.")
