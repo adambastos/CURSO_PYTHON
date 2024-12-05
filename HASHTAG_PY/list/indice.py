6814dc3a64efa45c08111d5585c6dad76cfe4265
Adam Bastos Santos <adam.santos@anvisa.gov.br>
2024-12-05 18:00:04 -0300

    último commit

--- HASHTAG_PY/list/indice.py
+++ HASHTAG_PY/list/indice.py
@@ -1,3 +1,5 @@
+#Descobrir a posição da geladeira, e com essa informação descobrir a posição da quantidade de geladeira no estoque
+
 produtos = ['TV', 'CELULAR', 'TABLET', 'MOUSE', 'TECLADO', 'GELADEIRA', 'FORNO']
 estoque = [100, 150, 100, 120, 70, 90, 80]
 
@@ -5,3 +7,10 @@ pos = produtos.index('GELADEIRA')
 qnt_estoque = estoque[pos]
 print(pos)
 print(qnt_estoque)
+
+
+nomeprod = input("Insira o nome do produto: ")
+if (not nomeprod in produtos):
+    print("Este produto não existe na lista.")
+elif (nomeprod in produtos):
+    print("{} conta na lista de produtos." .format(nomeprod))
